2a7bdda286dbfdacd7c9a6845e58bb9242368b8d
JacksonJones2005 <112198824+JacksonJones2005@users.noreply.github.com>
2022-09-23 14:00:28 -0500

    Code

--- Class-09/index.js
+++ Class-09/index.js
@@ -1,7 +1,7 @@
-fetch{""}
-.then{(response) => response.json()}
-.then{(data) => } {
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(response => response.json())
+.then((data) => {
     let imageElement = document.getElementById("random-image");
-    console.log(data.message);
+    console.log(data);
     imageElement.src = data.message;
 });
