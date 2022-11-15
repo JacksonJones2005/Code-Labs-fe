b892ffdd7ee888cd1b461aca6bfc5208d445fc16
JacksonJones2005 <112198824+JacksonJones2005@users.noreply.github.com>
2022-11-15 14:18:36 -0600

    Using the Components

--- Course Project/recipe-app/src/app/app.module.ts
+++ Course Project/recipe-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipieListComponent } from './recipes/recipie-list/recipie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipieListComponent
   ],
   imports: [
     BrowserModule
