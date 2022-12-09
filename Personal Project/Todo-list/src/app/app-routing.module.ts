c02b22e0ccbdf58d2c998761da862325ec375399
JacksonJones2005 <112198824+JacksonJones2005@users.noreply.github.com>
2022-12-09 13:59:19 -0600

    Add Navbar

--- Personal Project/Todo-list/src/app/app-routing.module.ts
+++ Personal Project/Todo-list/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",component: LandingPageComponent,pathMatch: 'full'
+  },
+  {
+    path:"todo-list",component:TodoListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Personal Project/Todo-list/src/app/app.module.ts
+++ Personal Project/Todo-list/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingPageComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
